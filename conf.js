3336c624626123a9a5df15baa67abf3a6af859d3
ctevnan <carolyntevnan@yahoo.com>
2016-05-31 15:16:54 -0400

    changed selenium address and name of file

---
+++ conf.js
@@ -0,0 +1,6 @@
+//configure
+exports.config = {
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['spec.js']
+}
--- config.js
+++
@@ -1,6 +0,0 @@
-//configure
-exports.config = {
-  framework: 'jasmine',
-  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
-  specs: ['calc-spec.js']
-}
