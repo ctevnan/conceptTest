3ca18edf3af63f5cecca0d23fea11bbd96b6797a
ctevnan <carolyntevnan@yahoo.com>
2016-05-31 14:50:08 -0400

    adding jasmine syntax to calc-spec.js to test title

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 //configure
 exports.config = {
   framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
   specs: ['calc-spec.js']
 }
